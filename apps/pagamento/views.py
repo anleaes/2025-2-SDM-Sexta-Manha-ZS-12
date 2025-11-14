f170c00e18abaf1abb7980e49ad3fac269cf752a
Lulizza <luizafioravanters@gmail.com>
2025-11-14 01:30:40 -0300

    serializer concluido

--- apps/pagamento/views.py
+++ apps/pagamento/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Pagamento
+from rest_framework import viewsets
+from .serializer import PagamentoSerializer
 
 # Create your views here.
+
+class PagamentoViewSet(viewsets.ModelViewSet):
+    queryset = Pagamento.objects.all()
+    serializer_class = PagamentoSerializer  
