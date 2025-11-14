994225d47d10468c5a8386c53862b39b7498fdca
Lulizza <luizafioravanters@gmail.com>
2025-11-14 01:49:25 -0300

    serializer concluido

---
+++ apps/documento/serializer.py
@@ -0,0 +1,7 @@
+from .models import Documento
+from rest_framework import serializers
+
+class DocumentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Documento
+        fields = '__all__'
