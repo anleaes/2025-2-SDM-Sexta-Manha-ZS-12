d6a9adcc48202b8b9f3b596d7efc853bbeab210d
Lulizza <luizafioravanters@gmail.com>
2025-11-14 01:17:33 -0300

    serializer concluido

---
+++ apps/fornecedor/serializer.py
@@ -0,0 +1,7 @@
+from .models import Fornecedor
+from rest_framework import serializers
+
+class FornecedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fornecedor
+        fields = '__all__'
