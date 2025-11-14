3068c03a8def6bacca6b6a454fda268c90701b94
Lulizza <luizafioravanters@gmail.com>
2025-11-14 01:27:03 -0300

    models pronto

--- apps/pagamento/models.py
+++ apps/pagamento/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from contrato.models import Contrato
 # Create your models here.
+
+class Pagamento(models.Model):
+    dataPagamento = models.DateField('Data', max_length=10)
+    valorPago = models.DecimalField('valorPago', max_length=10)
+    metodoPagamento = models.CharField('metodoPagamento', max_length=20, default='cartao', choices=[
+        ('boleto', 'Boleto'),
+        ('cartao', 'Cartão de Crédito'),
+        ('pix', 'PIX'),])
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Pagamento'
+        verbose_name_plural = 'Pagamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return "%s" % (self.cliente) 
