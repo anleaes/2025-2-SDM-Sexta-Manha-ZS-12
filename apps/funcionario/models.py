74916a109f6dd1573e96f84811dc942e88d2ab27
Lulizza <luizafioravanters@gmail.com>
2025-11-14 00:54:20 -0300

    models concluido

--- apps/funcionario/models.py
+++ apps/funcionario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Funcionario(models.Model):
+    nome = models.CharField('nome', max_length=50)
+    email = models.EmailField('email', max_length=50)
+    senha = models.CharField('senha', max_length=15)
+    cargo = models.CharField('cargo', default='efetivado', choices=['estagiario', 'jovem aprendiz', 'efetivado', 'Estagiario', 'Jovem aprendiz', 'Efetivado'])
