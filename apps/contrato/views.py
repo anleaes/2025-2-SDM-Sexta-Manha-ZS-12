b7fb8cd271dcd020b1943dfe02f00f7a9c58e395
Lulizza <luizafioravanters@gmail.com>
2025-11-14 00:37:53 -0300

    views concluida

--- apps/contrato/views.py
+++ apps/contrato/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Contrato
+from rest_framework import viewsets
+from .serializer import ContratoSerializer
 
 # Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Contrato.objects.all()
+    serializer_class = ContratoSerializer  
